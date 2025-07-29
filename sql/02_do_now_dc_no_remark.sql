2ab3ddb0c242c3063ba992e6b8d87e155f223b57
T-Chawanrat <Thip.Chawanrat@gmail.com>
2025-07-29 16:15:35 +0700

    add count warehouse

--- sql/02_do_now_dc_no_remark.sql
+++ sql/02_do_now_dc_no_remark.sql
@@ -1,4 +1,8 @@
-SELECT *, COUNT(*) OVER() AS total
+SELECT 
+  *, 
+  COUNT(*) OVER() AS total,
+  COUNT(CASE WHEN warehouse_id = 15 THEN 1 END) OVER() AS count_warehouse_15,
+  COUNT(CASE WHEN warehouse_id != 15 THEN 1 END) OVER() AS count_warehouse_not_15
 FROM trantech_bi.v02_do_now_dc_no_remark
 WHERE __WHERE_CLAUSE__
 LIMIT __LIMIT__ OFFSET __OFFSET__
